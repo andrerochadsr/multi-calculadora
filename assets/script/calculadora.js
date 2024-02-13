844e863c64359b0f55f9d8781e10dc2f41922bbc
Andre Rocha <andre.felipedsr@gmail.com>
2024-02-13 14:26:31 -0300

    asdsa

---
+++ assets/script/calculadora.js
@@ -0,0 +1,8 @@
+function visorAdd(){
+    document.querySelector('.input-tela').value += 8;
+}
+function mostrar() {
+    let visor = document.querySelector('#input-tela');
+    console.log(visor)
+}
+mostrar()
