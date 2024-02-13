65c67d8b41b328d154e8eb43da8c0b8b7279d7d9
Andre Rocha <andre.felipedsr@gmail.com>
2024-02-13 15:22:19 -0300

    att

--- assets/script/calculadora.js
+++ assets/script/calculadora.js
@@ -1,8 +1,19 @@
-function visorAdd(){
-    document.querySelector('.input-tela').value += 8;
-}
 function mostrar() {
-    let visor = document.querySelector('#input-tela');
+    let visor = document.querySelector('.input-tela').value;
+}
+
+function visorAdd(botao){
+    let elemento = botao
+    document.querySelector('.input-tela').value += elemento
+}
+
+function visorAddParen(arg){
+    if (arg ==='esq') {document.querySelector('.input-tela').value += '('}
+    if (arg ==='dir') {document.querySelector('.input-tela').value += ')';}
+}
+
+function visorDel() {
+    let visor = document.querySelector('.input-tela').value;
+    visor.pop()
     console.log(visor)
 }
-mostrar()
