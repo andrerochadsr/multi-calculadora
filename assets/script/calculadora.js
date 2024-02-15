5174fcecaf03ed9518c2657519c22643fcab46a9
Andre Rocha <andre.felipedsr@gmail.com>
2024-02-15 15:31:35 -0300

    att finalização

--- assets/script/calculadora.js
+++ assets/script/calculadora.js
@@ -1,19 +1,22 @@
-function mostrar() {
-    let visor = document.querySelector('.input-tela').value;
-}
 
+function resultado() {
+    let visor = document.querySelector('.input-tela').value;
+    try {
+        let opera = eval(visor)
+        document.querySelector('.input-tela').value = opera
+    } catch {
+        document.querySelector('.input-tela').value = 'ERRO!'
+    }
+}
 function visorAdd(botao){
     let elemento = botao
     document.querySelector('.input-tela').value += elemento
 }
-
 function visorAddParen(arg){
-    if (arg ==='esq') {document.querySelector('.input-tela').value += '('}
+    if (arg ==='esq') {document.querySelector('.input-tela').value += '(';}
     if (arg ==='dir') {document.querySelector('.input-tela').value += ')';}
 }
-
 function visorDel() {
     let visor = document.querySelector('.input-tela').value;
-    visor.pop()
-    console.log(visor)
+    document.querySelector('.input-tela').value = visor.slice(0, visor.length -1)
 }
